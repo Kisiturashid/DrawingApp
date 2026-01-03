c0789ecb6e2acd08bca1621871a56377bb39c837
unknown <kisiturashid01@gmail.com>
2026-01-03 10:19:29 +0300

    the 1st code for backend code

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,5 +1,45 @@
 package com.example.drawingapp
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context,attrs){
+    // drawing path
+    private lateinit var  drawPath: FingerPath
+
+    // defines what how to draw
+    private lateinit var canvasPaint: Paint
+
+    // defines how to draw
+    private lateinit var drawPaint: Paint
+    private var color = Color.BLACK
+    private lateinit var canvas: Canvas
+    private lateinit var canvasBitmap: Bitmap
+    private var brushSize:Float = 0.toFloat()
+
+    init {
+        setDrawing()
+    }
+
+    private fun setDrawing() {
+        drawPaint = Paint()
+        drawPath  = FingerPath(color, brushSize)
+        drawPaint.color = color
+        drawPaint.style = Paint.Style.STROKE
+        drawPaint.strokeJoin = Paint.Join.ROUND
+        drawPaint.strokeCap = Paint.Cap.ROUND
+        brushSize = 20.toFloat()
+
+    }
+
+
+    internal inner class FingerPath(val color:Int, val brushThickness: Float): Path()
+
+
+}
