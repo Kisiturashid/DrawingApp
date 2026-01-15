42be802289220dde61f549ac14959e1a18b2eb67
unknown <kisiturashid01@gmail.com>
2026-01-15 13:00:17 +0300

    The brush code for selecting the brushSize from the Dialog is done

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.drawingapp
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,15 +16,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
+        val  brush : ImageButton  = findViewById(R.id.ib_brush)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+        brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+
+
 
 
     }
     private fun showBrushSizeChooserDialog(){
         val brushDialog = Dialog(this)
-       // brushDialog.setContentView(R.layout.brush_size)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush Size: ")
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small)
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium)
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large)
 
-        // val smallBtn = brushDialog.ib_small
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        largeBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
+
+
 
     }
 }
